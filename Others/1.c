6699225a895325cddea1cdb38f4d1877d8cec6b5
Anastasiya <37482238+ovinnikova@users.noreply.github.com>
2018-04-13 18:22:32 +0300

    Create 1.c

---
+++ Others/1.c
@@ -0,0 +1,25 @@
+//* A program that inputs for a string of characters
+//* and outputs every character +2
+//* (Exmp: 123 -> 345; 567 -> 789; 163 -> 385 etc.)
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    string s = get_string("");
+
+
+ for (int i = 0, n = strlen(s); i < n; i++)
+        {
+                int j = s[i] + 2;
+                printf("%c ", j);
+            }
+
+        {
+            printf ("\n");
+        }
+
+}
