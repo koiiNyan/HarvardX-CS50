cce4ad03b56ecbd820730f2ab1250d5ba04930f6
Anastasiya <37482238+ovinnikova@users.noreply.github.com>
2018-04-16 22:54:21 +0300

    Update 1.c

--- Others/1.c
+++ Others/1.c
@@ -1,6 +1,6 @@
 //* A program that inputs for a string of characters
 //* and outputs every character +2
-//* (Exmp: 123 -> 345; 567 -> 789; 163 -> 385 etc.)
+//* (Exmp: 123 -> 345; 567 -> 789; 163 -> 385; abc -> cde etc.)
 
 #include <cs50.h>
 #include <stdio.h>
