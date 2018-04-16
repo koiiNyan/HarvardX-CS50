da96e2acbae62b9bb12f5cd4a2ade84b96160996
Anastasiya <37482238+ovinnikova@users.noreply.github.com>
2018-04-16 23:31:43 +0300

    Create 4.c

---
+++ Others/4.c
@@ -0,0 +1,34 @@
+//A program that prints out each 3rd letter of the word in the string, if it is a digit, there goes a smiley(s), if there's a blank -
+//the program understands that there goes a new word and starts
+//counting the 3rd letter of the next word.
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    string s = get_string("");
+
+    for (int i = 0, n = strlen(s); i < n; i++)
+
+        {
+            if isalpha(s[i])
+            {
+                i = i + 2;
+            printf("%c", s[i]);
+            }
+
+            else if isdigit(s[i])
+            {
+                printf (":c ");
+            }
+
+            else
+            {
+                printf ("%c", s[i]);
+            }
+        }
+
+}
