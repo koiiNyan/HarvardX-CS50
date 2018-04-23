c253f12ac9f0f87b7f9f8e063e19fb96b4197eb9
Anastasiya <37482238+ovinnikova@users.noreply.github.com>
2018-04-23 15:02:13 +0300

    Update 13.c

--- Others/13.c
+++ Others/13.c
@@ -8,40 +8,40 @@ printf("Birthmonth: \n");
     scanf("%i", &m);
 
     if (m==1)
-    printf ("Your bithmonth is January!\n");
+    printf ("Your birthmonth is January!\n");
 
     else if (m==2)
-    printf ("Your bithmonth is February!\n");
+    printf ("Your birthmonth is February!\n");
 
       else if (m==3)
-    printf ("Your bithmonth is March!\n");
+    printf ("Your birthmonth is March!\n");
 
       else if (m==4)
-    printf ("Your bithmonth is April!\n");
+    printf ("Your birthmonth is April!\n");
 
       else if (m==5)
-    printf ("Your bithmonth is May!\n");
+    printf ("Your birthmonth is May!\n");
 
       else if (m==6)
-    printf ("Your bithmonth is June!\n");
+    printf ("Your birthmonth is June!\n");
 
       else if (m==7)
-    printf ("Your bithmonth is July!\n");
+    printf ("Your birthmonth is July!\n");
 
       else if (m==8)
-    printf ("Your bithmonth is August!\n");
+    printf ("Your birthmonth is August!\n");
 
       else if (m==9)
-    printf ("Your bithmonth is September!\n");
+    printf ("Your birthmonth is September!\n");
 
       else if (m==10)
-    printf ("Your bithmonth is October!\n");
+    printf ("Your birthmonth is October!\n");
 
       else if (m==11)
-    printf ("Your bithmonth is November!\n");
+    printf ("Your birthmonth is November!\n");
 
   else if (m==12)
-    printf ("Your bithmonth is December!\n");
+    printf ("Your birthmonth is December!\n");
     else
     printf ("Type your birthmonth using a number\n");
 }
