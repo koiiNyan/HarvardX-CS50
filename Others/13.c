7718e9fcf418ccc41265bf79e7fa67c303086109
Anastasiya <37482238+ovinnikova@users.noreply.github.com>
2018-04-23 15:01:24 +0300

    Create 13.c

---
+++ Others/13.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main (void)
+{
+int m= 0;
+
+printf("Birthmonth: \n");
+    scanf("%i", &m);
+
+    if (m==1)
+    printf ("Your bithmonth is January!\n");
+
+    else if (m==2)
+    printf ("Your bithmonth is February!\n");
+
+      else if (m==3)
+    printf ("Your bithmonth is March!\n");
+
+      else if (m==4)
+    printf ("Your bithmonth is April!\n");
+
+      else if (m==5)
+    printf ("Your bithmonth is May!\n");
+
+      else if (m==6)
+    printf ("Your bithmonth is June!\n");
+
+      else if (m==7)
+    printf ("Your bithmonth is July!\n");
+
+      else if (m==8)
+    printf ("Your bithmonth is August!\n");
+
+      else if (m==9)
+    printf ("Your bithmonth is September!\n");
+
+      else if (m==10)
+    printf ("Your bithmonth is October!\n");
+
+      else if (m==11)
+    printf ("Your bithmonth is November!\n");
+
+  else if (m==12)
+    printf ("Your bithmonth is December!\n");
+    else
+    printf ("Type your birthmonth using a number\n");
+}
